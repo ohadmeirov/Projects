31ac7a1db89624bc53ae6f9e345aa6f4d804937f
MemoryGame <ohadmeirov1606@gmail.com>
2025-05-29 18:05:48 +0300

    Remove old 'React Node Mongo' folder after renaming project

--- React Node Mongo/TipTrip/src/config/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    port: process.env.PORT || 5000,
-    jwtExpiration: '24h',
-    clientURL: process.env.CLIENT_URL || 'http://localhost:3000'
-};
--- React Node Mongo/TipTrip/src/routes/stats.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statsController = require('../controllers/statsController');
-
-router.get('/posts-per-month', statsController.postsPerMonth);
-router.get('/users-per-month', statsController.usersPerMonth);  // שינוי הנתיב
-
-module.exports = router;
